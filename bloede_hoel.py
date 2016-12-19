9204b410772b6a8f176cb21303514a110d631b81
gaurwaithmelui <strategy155@gmail.com>
2016-12-19 15:37:21 +0300

    finished exam v 2

--- bloede_hoel.py
+++ bloede_hoel.py
@@ -4,20 +4,23 @@ filename = 'rus_nouns_parsed.txt'
 
 with open(filename, 'r') as f:
     all_strs = f.read().split('\n')
+diff = 0
 with open('sql.txt', 'w') as fo:
-    for idx,elem in enumerate(all_strs):
+    for elem in all_strs:
+        diff+=1
         without_vars = re.sub('\((.*?)\)','', elem)
         try:
             all_vars = without_vars.split('{')[1].split('|')
         except IndexError:
-            idx-=1
+            diff-=1
             continue
         all_lemmas = []
         for var in all_vars:
             all_lemmas.append(var.split('=')[0])
             dif_lemmas = list(set(all_lemmas))
         wordform = without_vars.split('{')[0]
-        for idx2, lemma in enumerate(dif_lemmas):
-            true_idx = idx+idx2 - 1
+        for lemma in dif_lemmas:
             fo.write('INSERT INTO rus_words (id, wordform, lemma)' + '\n')
-            fo.write('VALUES (' + str(true_idx) + ', ' + wordform + ', ' + lemma + ')' + '\n')
+            fo.write('VALUES (' + str(diff) + ', ' + wordform + ', ' + lemma + ')' + '\n')
+            diff+=1
+        diff-=1
